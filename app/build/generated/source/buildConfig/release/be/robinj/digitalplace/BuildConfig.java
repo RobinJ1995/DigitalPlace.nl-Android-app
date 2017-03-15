bd5a4c20c17af6d6f6695eb364f0bf99916a297d
Robin Jacobs <robin@robinj.be>
2017-03-15 18:49:42 +0000

    Fixed android support library version

---
+++ app/build/generated/source/buildConfig/release/be/robinj/digitalplace/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package be.robinj.digitalplace;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "be.robinj.digitalplace";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 6;
+  public static final String VERSION_NAME = "2.2.1";
+}
